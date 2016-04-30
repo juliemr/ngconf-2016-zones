83b6d2952025b3818675388bbccd7a68d9769206
Julie Ralph <ju.ralph@gmail.com>
2016-04-30 15:39:00 -0700

    Clear out old files

--- src/app/bootstrap.ts
+++
@@ -1,7 +0,0 @@
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {UserService} from './user-service';
-import {AppComponent} from './app-component';
-import {LoginService} from './login-service';
-
-
-bootstrap(AppComponent, [LoginService, UserService]);
